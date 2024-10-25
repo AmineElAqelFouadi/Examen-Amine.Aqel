f5ae2bd413e1e86a434f97cb11f3a9b18ee85762
Asrok <53124754+Asrok@users.noreply.github.com>
2024-10-25 09:08:21 +0200

    2pa

--- script.js
+++ script.js
@@ -1,19 +1,40 @@
-const SWAPI_BASE_URL = 'https://pokeapi.co/api/v2/pokemon'
+const SWAPI_BASE_URL = 'https://pokeapi.co/api/v2/pokemon/?limit=151'
 
 window.onload = async function () {
-    const pokemns = await getAllPokemons();
+    const pokemons = await getAllPokemons();
     const pokemonList = document.getElementById('pokemon-list');
-    for (const pokemon of pokemns) {
+    pokemonList.innerHTML = '';
+    for (const pokemon of pokemons) {
         const listItem = document.createElement('li');
         listItem.innerText = pokemon.name;
+        listItem.addEventListener('click', async function() {
+            const pokemonDetails = await getPokemonDetails(pokemon.url);
+            displayPokemonDetails(pokemonDetails);
+        });
         pokemonList.appendChild(listItem);
     }
-    
 };
 
 async function getAllPokemons() {
-    const response = await fetch(`${SWAPI_BASE_URL}/?limit=151`);  
-    const jRsponse = await response.json();
-    const pokemonsArray = jRsponse.results.name;
+    const response = await fetch(`${SWAPI_BASE_URL}`);
+    const jsonResponse = await response.json();
+    const pokemonsArray = jsonResponse.results;
     return pokemonsArray;
+}
+
+async function getPokemonDetails(url) {
+    const response = await fetch(url);
+    const pokemonDetails = await response.json();
+    return pokemonDetails;
+}
+
+function displayPokemonDetails(pokemonDetails) {
+    const pokemonContainer = document.getElementById('pokemon-container');
+    pokemonContainer.innerHTML = `
+    <h2>${pokemonDetails.name}</h2>
+    <p>ID: ${pokemonDetails.id}</p>
+    src="${pokemon.sprites.other['official-artwork'].front_default}" alt="${pokemon.name}">
+    <p>Tipos: ${pokemon.types.map(typeInfo => typeInfo.type.name).join(', ')}</p>
+    `
+    ;
 }
